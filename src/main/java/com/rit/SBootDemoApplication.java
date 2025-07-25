45ec90dddfbddbc2c10b4ad77089c43e8f63f9d8
Global Developer <ritit.programs@gmail.com>
2025-07-25 19:28:09 +0530

    aa

---
+++ src/main/java/com/rit/SBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.rit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SBootDemoApplication.class, args);
+	}
+
+}
